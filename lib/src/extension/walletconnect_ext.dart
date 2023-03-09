c321b672235fdcd648908d16dc50a66eca8c8cb2
Samuel Ezedi <60943943+samuelezedi@users.noreply.github.com>
2023-03-09 13:00:16 +0100

    wallet list edited

--- lib/src/extension/walletconnect_ext.dart
+++ lib/src/extension/walletconnect_ext.dart
@@ -2,12 +2,12 @@ import 'package:walletconnect_dart/walletconnect_dart.dart';
 import 'package:walletconnect_qrcode_modal_dart/src/models/wallet.dart';
 
 extension WCExtension on WalletConnect {
-  static Wallet? _walletCred;
+  static List<Wallet>? _walletCred;
 
-  Wallet? get wallet => _walletCred;
+  List<Wallet>? get wallet => _walletCred;
 
   void putSelectedWallet(Wallet wallet) {
-    _walletCred = wallet;
+    _walletCred?.add(wallet);
   }
 
 }
